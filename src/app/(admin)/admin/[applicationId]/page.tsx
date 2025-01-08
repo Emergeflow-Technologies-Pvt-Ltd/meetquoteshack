c0a465e967c458f15fdd8ceaf561329196d43208
Tarek Ahmed <tarek5701@gmail.com>
2025-01-08 11:38:55 -0700

    refactor

--- src/app/(admin)/admin/[applicationId]/page.tsx
+++ src/app/(admin)/admin/[applicationId]/page.tsx
@@ -21,6 +21,7 @@ import {
   SelectValue,
 } from "@/components/ui/select";
 import axios from "axios";
+import { getStatusColors } from "@/lib/utils";
 
 interface Props {
   params: Promise<{
@@ -34,19 +35,6 @@ type ApplicationWithUser = MortgageApplication & {
   documentKey?: string;
   estimatedPropertyValue?: number;
   intendedPropertyAddress?: string;
-};
-
-export const getStatusColors = (status: string) => {
-  const colors = {
-    VERIFIED: "bg-green-100 text-green-800 border-green-300",
-    REJECTED: "bg-red-100 text-red-800 border-red-300",
-    PROCESSING: "bg-yellow-100 text-yellow-800 border-yellow-300",
-    PROGRESSING: "bg-yellow-100 text-yellow-800 border-yellow-300",
-    APPROVED: "bg-green-100 text-green-800 border-green-300",
-    UPLOADED: "bg-blue-100 text-blue-800 border-blue-300",
-    default: "bg-gray-100 text-gray-800 border-gray-300",
-  };
-  return colors[status as keyof typeof colors] || colors.default;
 };
 
 export default function ApplicationPage({ params }: Props) {
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,17 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+
+export const getStatusColors = (status: string) => {
+  const colors = {
+    VERIFIED: "bg-green-100 text-green-800 border-green-300",
+    REJECTED: "bg-red-100 text-red-800 border-red-300",
+    PROCESSING: "bg-yellow-100 text-yellow-800 border-yellow-300",
+    PROGRESSING: "bg-yellow-100 text-yellow-800 border-yellow-300",
+    APPROVED: "bg-green-100 text-green-800 border-green-300",
+    UPLOADED: "bg-blue-100 text-blue-800 border-blue-300",
+    default: "bg-gray-100 text-gray-800 border-gray-300",
+  };
+  return colors[status as keyof typeof colors] || colors.default;
+};
