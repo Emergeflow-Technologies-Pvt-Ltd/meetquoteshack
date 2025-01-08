5ab082860372e5e365e639b99169c3f890945f47
Tarek Ahmed <tarek5701@gmail.com>
2025-01-08 14:34:25 -0700

    fixed supabase client

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient(
-  "https://hqeodauepgtvwuvzgfpq.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhxZW9kYXVlcGd0dnd1dnpnZnBxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTAxMjQ3OTQsImV4cCI6MjAyNTcwMDc5NH0.M",
+  process.env.NEXT_PUBLIC_SUPABASE_URL as string,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string,
 );
+
