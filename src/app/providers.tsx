486b9ed4790381af670cd96d40f50b912f25a3b9
prime7 <tarek5701@gmail.com>
2025-05-05 21:53:09 -0600

    added chat

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,7 +1,19 @@
 'use client'
 
 import { SessionProvider } from "next-auth/react";
+import Script from 'next/script';
 
 export default function Providers({ children }: { children: React.ReactNode }) {
-  return <SessionProvider>{children}</SessionProvider>;
+  return (
+    <SessionProvider>
+      <>
+        {children}
+        <Script
+          id="tawkto"
+          strategy="afterInteractive"
+          src="https://embed.tawk.to/681984ce5510d619105dcd59/1iqhqthvs"
+        />
+      </>
+    </SessionProvider>
+  );
 }
