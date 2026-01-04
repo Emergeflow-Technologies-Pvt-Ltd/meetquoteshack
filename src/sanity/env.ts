b7b52f44a6582ae8b17f91126a0147080ca3b216
Swapnil Kale <swapnil.pavesoft@gmail.com>
2026-01-04 18:09:52 +0530

    chore: stripe + sanity env update

--- src/sanity/env.ts
+++ src/sanity/env.ts
@@ -1,7 +1,13 @@
 export const apiVersion =
-  process.env.NEXT_PUBLIC_SANITY_API_VERSION || "2024-12-10";
+  process.env.NEXT_PUBLIC_SANITY_API_VERSION ?? "2023-10-01";
 
-export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
+export const dataset =
+  process.env.NEXT_PUBLIC_SANITY_DATASET ?? "production";
+
 export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
 
-export const useCdn = false;
+if (!projectId) {
+  throw new Error("Missing NEXT_PUBLIC_SANITY_PROJECT_ID");
+}
+
+export const useCdn = process.env.NODE_ENV === "production";
