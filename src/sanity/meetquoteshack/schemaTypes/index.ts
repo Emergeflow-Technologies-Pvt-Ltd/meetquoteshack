7643307c0723f5be06450ca8cfaa08e3fb652969
soham-pavesoft <soham.pavesoft@gmail.com>
2026-01-08 15:15:53 +0530

    [Deletion] Deleted  redundant sanity project

--- src/sanity/meetquoteshack/schemaTypes/index.ts
+++
@@ -1,6 +0,0 @@
-import { author } from "./author";
-import { blockContent } from "./blockContent";
-import { category } from "./category";
-import { blog } from "./blog";
-
-export const schemaTypes = [author, blog, category,blockContent];
