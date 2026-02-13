8f2d502acd0e97aef52d752bf17a765d662e424d
Quirky Devil <81952913+QuirkyDevil@users.noreply.github.com>
2026-02-13 12:57:06 +0530

    hotfix: sanity lib deprecated default export

--- src/sanity/lib/image.ts
+++ src/sanity/lib/image.ts
@@ -1,5 +1,6 @@
-import createImageUrlBuilder from "@sanity/image-url";
+import {createImageUrlBuilder} from "@sanity/image-url";
 import type { SanityImageSource } from "@sanity/image-url";
+
 
 import { dataset, projectId } from "../env";
 
